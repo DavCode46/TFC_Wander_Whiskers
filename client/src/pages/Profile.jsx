24abe944765296b5e09925997aa98e39064fb596
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-05-01 18:00:00 +0200

    Feat: Profile page finished

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import ChakraTab from "./ChakraTab";
+
+const Profile = () => {
+  return (
+    <section className="w-full p-10 m-auto">
+      <ChakraTab />
+    </section>
+  );
+};
+
+export default Profile;
