7edb89f71671a770e1bd89e8e7c2b1fc0cb66d53
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-05-01 17:56:54 +0200

    Feat: Admin deletePôst finished

---
+++ client/src/pages/DeletePost.jsx
@@ -0,0 +1,108 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { UserContext } from "../context/userContext";
+import axios from "axios";
+import { Button, message, Popconfirm } from "antd";
+import { DeleteOutlined, QuestionCircleOutlined } from "@ant-design/icons";
+
+import { CircularProgress } from "@chakra-ui/react";
+
+const DeletePost = ({ postID: id }) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("Error al eliminar la publicación");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const { currentUser } = useContext(UserContext);
+  const token = currentUser?.token;
+  useEffect(() => {
+    if (!token) navigate("/login");
+  }, []);
+  const deletePost = async () => {
+    try {
+      const res = await axios.delete(
+        `${import.meta.env.VITE_REACT_APP_URL}/posts/${id}`,
+        {
+          withCredentials: true,
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      if (res.status === 200) {
+        if (
+          location.pathname === `/profile/${currentUser?.id}` ||
+          location.pathname === "/dashboard"
+        ) {
+          // navigate(`/profile/${currentUser?.id}`);
+          success();
+          window.location.reload();
+        } else {
+          success();
+          navigate("/posts");
+        }
+      }
+      setLoading(false);
+    } catch (err) {
+      setError(err.response.data.message);
+      errorMessage();
+      console.log(err);
+    }
+  };
+
+  const success = () => {
+    messageApi.open({
+      type: "success",
+      content: "Usuario eliminado con éxito",
+    });
+  };
+
+  const errorMessage = () => {
+    messageApi.open({
+      type: "error",
+      content: error,
+    });
+  };
+
+  if (loading)
+    return (
+      <CircularProgress
+        isIndeterminate
+        size="100px"
+        thickness="7px"
+        aria-label="cargando"
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      />
+    );
+  return (
+    <>
+      {contextHolder}
+      <Popconfirm
+        title="Eliminar publicación"
+        description="¿Estás seguro que deseas eliminar la publicación?"
+        icon={
+          <QuestionCircleOutlined
+            style={{
+              color: "red",
+            }}
+          />
+        }
+        okText="Sí"
+        cancelText="No"
+        okType="danger"
+        onConfirm={deletePost}
+      >
+        <Button danger icon={<DeleteOutlined />}>
+          Eliminar
+        </Button>
+      </Popconfirm>
+    </>
+  );
+};
+
+export default DeletePost;
