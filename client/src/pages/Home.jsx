762a239c944e947b4b4f357c2e8a690f4b66ffa4
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-05-01 17:58:04 +0200

    Feat: Home page finished

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Hero from '@components/Hero';
+import HomeFound from '@components/HomeFound';
+const Home = () => {
+  
+
+  return (
+    <div>
+      <Hero />
+      <HomeFound />
+    </div>
+  )
+};
+
+export default Home;
