116102eb43bf26d58e234ec1c5c7d76d18f45a3e
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-05-01 17:37:15 +0200

    Feat: structure frontend created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "./theme/theme";
+
+import CustomLayout from "./components/CustomLayout";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import PostsPage from "./pages/PostsPage";
+import EditPost from "./pages/EditPost";
+import DeletePost from "./pages/DeletePost";
+import PostDetail from "./pages/PostDetail";
+import LocationPosts from "./pages/LocationPosts";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Creator from "./pages/Creator";
+import Register from "./pages/Register";
+import Logout from "./pages/Logout";
+import NotFoundPage from "./pages/NotFound";
+import Contact from "./pages/Contact";
+import Help from "./pages/Help";
+import PostsBySpecie from "./pages/PostsBySpecie";
+
+import UserProvider from "./context/userContext"; // Importa UserProvider desde el archivo UserContext
+import PostCreator from "./components/PostCreator";
+import Dashboard from "./pages/Dashboard";
+import UsersManagement from "./pages/UsersManagement";
+// import ProfilePosts from "./components/ProfilePosts";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <ChakraProvider theme={theme}>
+        <UserProvider>
+          <CustomLayout />
+        </UserProvider>
+      </ChakraProvider>
+    ),
+    errorElement: <NotFoundPage />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/create-post", element: <CreatePost /> },
+      { path: "/posts", element: <PostsPage /> },
+      { path: "/posts/:id/edit", element: <EditPost /> },
+      { path: "/posts/:id/delete", element: <DeletePost /> },
+      { path: "/post/:id/detail", element: <PostDetail /> },
+      { path: "/profile/:id", element: <Profile /> },
+      { path: "/posts/location/:location", element: <LocationPosts /> },
+      // { path: "/posts/creator/:id", element: <Creator /> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+      { path: "/logout", element: <Logout /> },
+      { path: "/contact", element: <Contact /> },
+      { path: "/help", element: <Help /> },
+      { path: "posts/species/:specie", element: <PostsBySpecie />},
+      { path: "posts/users/:id", element: <Creator /> },
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/dashboard/users", element: <UsersManagement />}
+      // { path: "posts/users/:id", element: <ProfilePosts /> }
+    ],
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
