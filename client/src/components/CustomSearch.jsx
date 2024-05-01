1bb8d1b20f94ee8e4055418561aaf1b0de6b6f2a
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-05-01 17:44:57 +0200

    Feat: Custom search input finished

---
+++ client/src/components/CustomSearch.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const CustomSearch = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInputChange = (event) => {
+    const newSearchTerm = event.target.value;
+    console.log(newSearchTerm);
+    setSearchTerm(newSearchTerm);
+    if (onSearch) {
+      onSearch(newSearchTerm);
+    }
+  };
+
+  const handleSearch = () => {
+    if (onSearch) {
+      onSearch(searchTerm);
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div className="flex items-center border border-gray-300 rounded-md shadow-md overflow-hidden">
+      <input
+        className="px-4 py-2 flex-1 text-gray-700 focus:outline-none w-[20rem]"
+        type="text"
+        value={searchTerm}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyPress}
+        placeholder="Buscar por ubicación"
+      />
+      <button className="px-4 py-2 bg-color-btn text-white hover:bg-color-btnHover focus:outline-none">
+        Buscar
+      </button>
+    </div>
+  );
+};
+
+export default CustomSearch;
